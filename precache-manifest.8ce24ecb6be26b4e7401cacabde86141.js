cc6e7112cc8f54a1e4e7dca732d074abed92e76b
Rakim francis <rakimfrancis@Rakims-MBP-2.home>
2019-06-04 20:55:54 -0400

    Updates

---
+++ precache-manifest.8ce24ecb6be26b4e7401cacabde86141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1e0a86ea6c9af2b56a9b4441061b53",
+    "url": "/homework19/react-click-game/friendrefactor/index.html"
+  },
+  {
+    "revision": "70e6a081905e195b887e",
+    "url": "/homework19/react-click-game/friendrefactor/static/css/main.d08a38ab.chunk.css"
+  },
+  {
+    "revision": "1a7171200eb2526ec47d",
+    "url": "/homework19/react-click-game/friendrefactor/static/js/2.e12b31cc.chunk.js"
+  },
+  {
+    "revision": "70e6a081905e195b887e",
+    "url": "/homework19/react-click-game/friendrefactor/static/js/main.dcb89c5e.chunk.js"
+  },
+  {
+    "revision": "59fee3e70dcf3a68944b",
+    "url": "/homework19/react-click-game/friendrefactor/static/js/runtime~main.8de15db9.js"
+  }
+]);
